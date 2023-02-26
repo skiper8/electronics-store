657256ccdbf740e58cb598d1b707f2bd40935d09
askip <a.skiper2013@yandex.ru>
2023-02-26 04:21:48 +0300

    add test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,8 +5,11 @@ def test_Store():
     item = Store('Телефон', 100, 1)
     assert item.calculate_total_price() == 100
     assert item.apply_discount() == 100
-    item.name = 'СуперТелефон'
-    assert item.name == 'Телефон'
+    assert item.name != 'СуперТелефон'
+    item.price = 1000
+    assert item.price == 1000
+    item.amount = 10
+    assert item.amount == 10
     Store.instantiate_from_csv('test_items.csv')
     assert len(Store.all) == 6
     assert item.is_integer(1.6) == False
